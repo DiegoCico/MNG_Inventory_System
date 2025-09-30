64053a5b7298a83206260dce2f6906cde47c7757
DiegoCico <cicotosted@gmail.com>
2025-09-30 14:43:53 -0400

    need to install packages but eslint is set up

---
+++ src/frontend/eslint.config.mjs
@@ -0,0 +1,30 @@
+// eslint.config.mjs
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Not needed in React 17+
+      "react/jsx-uses-react": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**"],
+  },
+];
---
+++ src/frontend/readme.md
@@ -0,0 +1,6 @@
+NEED TO RUN THIS COMMAND:
+
+npm i -D eslint @eslint/js typescript-eslint eslint-plugin-react eslint-plugin-react-hooks
+
+
+issues on my new macbook cant install it
