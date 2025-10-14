7e51ac238c08ece8fc4f90deaebf82dc9004e6db
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-10-14 08:05:04 -0400

    rebase

--- src/api/src/routers/index.ts
+++ src/api/src/routers/index.ts
@@ -2,12 +2,13 @@ import { router, publicProcedure, mergeRouters } from './trpc';
 import { helloRouter } from './hello';
 import { usersRouter } from './users';
 import { s3Router } from './s3.options.router';
+import { authRouter } from "./auth";
 
 const coreRouter = router({
   health: publicProcedure.query(() => ({ ok: true })),
 });
 
-const routers = [coreRouter, helloRouter, usersRouter, s3Router];
+const routers = [coreRouter, helloRouter, usersRouter, s3Router, authRouter];
 
 export const appRouter = mergeRouters(...routers);
 
