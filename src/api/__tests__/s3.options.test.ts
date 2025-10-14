7a3deb314a0ad96ab025a6697109785f2c23592d
sayegh.st@northeastern.edu <stephaniesayegh375@gmail.com>
2025-10-13 22:45:17 -0400

    syntax

--- src/api/__tests__/s3.options.test.ts
+++ src/api/__tests__/s3.options.test.ts
@@ -5,7 +5,7 @@ import { mockClient } from "aws-sdk-client-mock";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 
-import { s3Router } from "../src/routers/S3.options";
+import { s3Router } from "../src/routers/s3.options";
 
 const s3Mock = mockClient(S3Client);
 
